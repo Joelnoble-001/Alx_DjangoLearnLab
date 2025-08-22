f468f434e02fa53c05f7c8d9b806e3afd13f4e5e
joel odunoye <joelodunoye@gmail.com>
2025-08-22 16:50:49 +0100

    update views.py, urls.py, forms.py, models.py and template folder

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,10 +13,8 @@ app_name = 'blog'
 urlpatterns = [
     # your other urls, e.g. path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-
     path('profile/', views.profile, name='profile'),
 
     path('', PostListView.as_view(), name='post-list'),
@@ -24,4 +22,8 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path("posts/<int:post_id>/comments/new/", views.add_comment, name="add-comment"),
+    path("comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
+    path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
