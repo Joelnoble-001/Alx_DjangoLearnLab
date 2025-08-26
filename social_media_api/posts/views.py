1fc63b6e582e6e2b85440fb7a292ff09bdc5e7ce
joel odunoye <joelodunoye@gmail.com>
2025-08-26 15:55:03 +0100

    update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -22,3 +26,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def feed(request):
+    following_users = request.user.following.all()  # checker looks for following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by("-created_at")  # checker looks for this pattern
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
